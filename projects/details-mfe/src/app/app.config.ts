68c8280359b9a0bde1561355669b4ffdf94efb7c
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-17 14:20:09 +0200

    Rename *.routes.ts file for each application

--- projects/details-mfe/src/app/app.config.ts
+++ projects/details-mfe/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { routes } from './app.routes';
+import { routes } from './details.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimations()],
