ccd44df5711d45865f80b39ef2f52f0117502190
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-17 15:21:28 +0200

    feat(mf): add dynamic shell routes for details-mfe & search-mfe in host.routes.ts

--- projects/auth-host-app/src/app/host.routes.ts
+++ projects/auth-host-app/src/app/host.routes.ts
@@ -1,3 +1,26 @@
+import { DETAILS_ROUTES } from './../../../details-mfe/src/app/details.routes';
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const HOST_ROUTES: Routes = [];
+export const HOST_ROUTES: Routes = [
+  { path: '', component: AppComponent },
+  {
+    path: 'details-mfe',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './routes',
+      }).then((m) => m.DETAILS_ROUTES),
+  },
+  {
+    path: 'search-mfe',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        exposedModule: './searchRoutes',
+      }).then((m) => m.SEARCH_ROUTES),
+  },
+];
