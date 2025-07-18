2d36108840f8c9e54e684e67d54dcbeaaa32bdcf
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-18 02:19:16 +0200

    Fix Init Inline errors Component

---
+++ projects/auth-host-app/src/app/components/inline-errors/inline-errors.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inline-errors',
+  standalone: true,
+  imports: [],
+  template: ``,
+})
+export class InlineErrorsComponent {}
