2b99598e5e64e8136528710c479bbc865925c282
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-18 02:19:46 +0200

    Init form error component

---
+++ projects/auth-host-app/src/app/components/form-error/form-error.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-error',
+  standalone: true,
+  imports: [],
+  template: ``,
+})
+export class FormErrorComponent {}
