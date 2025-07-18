7135982265a0a07c21ac003791612ceb47610fc0
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-18 02:25:35 +0200

    Init forgot-password page

---
+++ projects/auth-host-app/src/app/pages/forgot-password/forgot-password.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      forgot-password works!
+    </p>
+  `,
+  styleUrl: './forgot-password.component.scss'
+})
+export class ForgotPasswordComponent {
+
+}
