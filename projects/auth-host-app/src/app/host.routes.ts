e2195e42f1465aaae3e89967b2a7f21b8b14d725
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-19 16:27:50 +0200

    feat(router): update HOST_ROUTES with auth, unauth & resetToken guards and MFE lazy modules

--- projects/auth-host-app/src/app/host.routes.ts
+++ projects/auth-host-app/src/app/host.routes.ts
@@ -1,10 +1,56 @@
-import { DETAILS_ROUTES } from './../../../details-mfe/src/app/details.routes';
+// projects/auth-host-app/src/app/host.routes.ts
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import { LoginComponent } from './pages/login/login.component';
+import { unauthGuard } from './guards/unauth/unauth.guard';
+import { RegisterComponent } from './pages/register/register.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { authGuard } from './guards/auth/auth.guard';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { resetTokenGuard } from './guards/reset-token/reset-token.guard';
 
 export const HOST_ROUTES: Routes = [
-  { path: '', component: AppComponent },
+  // Redirect root URL to profile
+  { path: '', redirectTo: 'profile', pathMatch: 'full' },
+
+  // Public routes (only for unauthenticated users)
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [unauthGuard],
+  },
+
+  // Public routes (only for unauthenticated users)
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [unauthGuard],
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [unauthGuard],
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+    canActivate: [unauthGuard],
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent,
+    canActivate: [unauthGuard, resetTokenGuard],
+  },
+
+  // Protected route
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [authGuard],
+  },
+
+  // Lazy-loaded routes for micro frontends
   {
     path: 'details-mfe',
     loadChildren: () =>
