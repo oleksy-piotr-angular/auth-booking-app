9fd6646033db1c8f3e961143ba630af05d452b39
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-19 15:55:45 +0200

    feat(auth): implement functional unauthGuard with redirect for authenticated users

--- projects/auth-host-app/src/app/guards/unauth/unauth.guard.ts
+++ projects/auth-host-app/src/app/guards/unauth/unauth.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+// projects/auth-host-app/src/app/guards/unauth/unauth.guard.ts
 
-export const unauthGuard: CanActivateFn = (route, state) => {
-  return true;
+import { inject } from '@angular/core';
+import { Router, CanActivateFn } from '@angular/router';
+import { TokenService } from '../../services/token/token.service';
+
+export const unauthGuard: CanActivateFn = () => {
+  const token = inject(TokenService).getToken();
+  const router = inject(Router);
+
+  if (!token) {
+    return true;
+  }
+
+  router.navigate(['/']);
+  return false;
 };
