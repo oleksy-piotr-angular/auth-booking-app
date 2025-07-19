4d17bc4a76e0c9045c05ea0d6b7410a71a1da61e
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-19 21:48:54 +0200

    feat(nav): implement NavBarComponent with Material toolbar, routing links, and logout logic

--- projects/auth-host-app/src/app/components/nav-bar/nav-bar.component.ts
+++ projects/auth-host-app/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TokenService } from '../../services/token/token.service';
+import { SharedMaterialModule } from '@booking-app/shared-material';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive, SharedMaterialModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
+  title = 'auth-host-app';
+  constructor(private tokenService: TokenService, private router: Router) {}
 
+  get isAuthenticated(): boolean {
+    return !!this.tokenService.getToken();
+  }
+
+  logout(): void {
+    this.tokenService.clearToken();
+    this.router.navigate(['login']);
+  }
 }
