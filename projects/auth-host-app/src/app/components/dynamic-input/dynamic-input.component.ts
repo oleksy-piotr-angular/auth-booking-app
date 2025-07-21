599c017bef25ca5f24ed6a40a9bab2a58c989113
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-21 14:30:47 +0200

    Init Synamic Input Component

---
+++ projects/auth-host-app/src/app/components/dynamic-input/dynamic-input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-input',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      dynamic-input works!
+    </p>
+  `,
+  styles: ``
+})
+export class DynamicInputComponent {
+
+}
