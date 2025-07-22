e053305fd4aa468c6fdce030deb2bef6c3958247
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-22 14:47:46 +0200

    feat(login-page): implement component shell and template with dynamic-form

--- projects/auth-host-app/src/app/pages/login/login.component.ts
+++ projects/auth-host-app/src/app/pages/login/login.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DynamicFormComponent } from '../../components/dynamic-form/dynamic-form.component';
+import { FieldConfig } from '../../models/field-config.model';
+import { AuthService } from '../../services/auth/auth.service';
+import { LoginPayload } from '../../dtos/auth.dto';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  template: ` <p>login works!</p> `,
+  imports: [DynamicFormComponent],
+  template: ` <app-dynamic-form
+    [fields]="fields"
+    (submitForm)="onLogin($event)"
+  ></app-dynamic-form>`,
   styles: ``,
 })
 export class LoginComponent {
-  onLogin(arg0: { email: string; password: string }) {
-    throw new Error('Method not implemented.');
+  private auth: AuthService = inject(AuthService);
+
+  public fields: FieldConfig[] = [
+    {
+      name: 'username',
+      label: 'Username',
+      placeholder: 'Enter username',
+      validators: [], // will add Validators later
+      errorMessages: {},
+    },
+    {
+      name: 'password',
+      label: 'Password',
+      type: 'password',
+      validators: [],
+      errorMessages: {},
+    },
+  ];
+
+  public onLogin(credentials: LoginPayload): void {
+    this.auth.login(credentials).subscribe();
   }
 }
