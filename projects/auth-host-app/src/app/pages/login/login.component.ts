1cac5c8b7cc217366f771d23cc66307afae4d75f
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-22 15:40:03 +0200

    feat(login): wire Router, display FormErrorComponent, switch to CommonModule and email field | TEST GREEN

--- projects/auth-host-app/src/app/pages/login/login.component.ts
+++ projects/auth-host-app/src/app/pages/login/login.component.ts
@@ -1,28 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { DynamicFormComponent } from '../../components/dynamic-form/dynamic-form.component';
+import { FormErrorComponent } from '../../components/form-error/form-error.component';
 import { FieldConfig } from '../../models/field-config.model';
 import { AuthService } from '../../services/auth/auth.service';
 import { LoginPayload } from '../../dtos/auth.dto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DynamicFormComponent],
-  template: ` <app-dynamic-form
-    [fields]="fields"
-    (submitForm)="onLogin($event)"
-  ></app-dynamic-form>`,
+  imports: [CommonModule, DynamicFormComponent, FormErrorComponent],
+  template: `
+    <app-dynamic-form
+      [fields]="fields"
+      (submitForm)="onLogin($event)"
+    ></app-dynamic-form>
+    <app-form-error *ngIf="errorMsg" [message]="errorMsg"></app-form-error>
+  `,
   styles: ``,
 })
 export class LoginComponent {
   private auth: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
 
   public fields: FieldConfig[] = [
     {
-      name: 'username',
-      label: 'Username',
-      placeholder: 'Enter username',
-      validators: [], // will add Validators later
+      name: 'email',
+      label: 'Email',
+      placeholder: 'Enter email',
+      validators: [],
       errorMessages: {},
     },
     {
@@ -34,7 +41,12 @@ export class LoginComponent {
     },
   ];
 
+  public errorMsg: string | null = null;
+
   public onLogin(credentials: LoginPayload): void {
-    this.auth.login(credentials).subscribe();
+    this.auth.login(credentials).subscribe({
+      next: () => this.router.navigate(['/auth/profile']),
+      error: (err) => (this.errorMsg = err.error.message),
+    });
   }
 }
