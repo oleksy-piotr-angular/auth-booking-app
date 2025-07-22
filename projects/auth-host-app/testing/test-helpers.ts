df23b2a83c1f0971fce9ceaa411333311d8ee8b3
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-22 15:28:17 +0200

    chore(test-helpers): add createAuthServiceSpy & provideMockAuthService; rename token helper

--- projects/auth-host-app/testing/test-helpers.ts
+++ projects/auth-host-app/testing/test-helpers.ts
@@ -1,4 +1,5 @@
 import { Router } from '@angular/router';
+import { AuthService } from '../src/app/services/auth/auth.service';
 import { TokenService } from '../src/app/services/token/token.service';
 
 export function createRouterSpy() {
@@ -12,11 +13,22 @@ export function provideMockRouter(routerSpy?: jasmine.SpyObj<Router>) {
   };
 }
 
+export function createAuthServiceSpy() {
+  return jasmine.createSpyObj<AuthService>('AuthService', ['login']);
+}
+
+export function provideMockAuthService(authSpy?: jasmine.SpyObj<AuthService>) {
+  return {
+    provide: AuthService,
+    useValue: authSpy || createAuthServiceSpy(),
+  };
+}
+
 export function createTokenServiceSpy() {
   return jasmine.createSpyObj<TokenService>('TokenService', ['getToken']);
 }
 
-export function provideMockAuthService(
+export function provideMockTokenService(
   tokenSpy?: jasmine.SpyObj<TokenService>
 ) {
   return {
