ab31ae866e09561d2e29727dba6accf8dfa2f780
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-22 16:38:19 +0200

    feat(register): render DynamicFormComponent with email/password/confirmPassword fields

--- projects/auth-host-app/src/app/pages/register/register.component.ts
+++ projects/auth-host-app/src/app/pages/register/register.component.ts
@@ -1,19 +1,53 @@
-import { Component } from '@angular/core';
+// projects/auth-host-app/src/app/pages/register/register.component.ts
+
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { DynamicFormComponent } from '../../components/dynamic-form/dynamic-form.component';
+import { FieldConfig } from '../../models/field-config.model';
+import { AuthService } from '../../services/auth/auth.service';
+import { RegisterPayload } from '../../dtos/auth.dto';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
-  template: ` <p>register works!</p> `,
-  styles: ``,
+  imports: [CommonModule, DynamicFormComponent],
+  template: `
+    <app-dynamic-form
+      [fields]="fields"
+      (submitForm)="onRegister($event)"
+    ></app-dynamic-form>
+  `,
 })
 export class RegisterComponent {
-  onRegister(badValue: {
-    name: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-  }) {
-    throw new Error('Method not implemented.');
+  private auth = inject(AuthService);
+
+  public fields: FieldConfig[] = [
+    {
+      name: 'email',
+      label: 'Email',
+      placeholder: 'Enter email',
+      validators: [],
+      errorMessages: {},
+    },
+    {
+      name: 'password',
+      label: 'Password',
+      placeholder: 'Enter password',
+      type: 'password',
+      validators: [],
+      errorMessages: {},
+    },
+    {
+      name: 'confirmPassword',
+      label: 'Confirm Password',
+      placeholder: 'Re-enter password',
+      type: 'password',
+      validators: [],
+      errorMessages: {},
+    },
+  ];
+
+  public onRegister(_: RegisterPayload): void {
+    // implementation to come in next steps
   }
 }
