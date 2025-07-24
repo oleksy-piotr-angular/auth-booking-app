a8738be45deb404b7e742ed6f639de2592f87ee3
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-24 15:18:21 +0200

    test(register): add full RegisterComponent spec with confirm‐password logic

--- projects/auth-host-app/src/app/pages/register/register.component.ts
+++ projects/auth-host-app/src/app/pages/register/register.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
+import { FormFieldConfig } from '../../models/field-config.model';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      register works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>register works!</p> `,
+  styles: ``,
 })
 export class RegisterComponent {
-
+  formConfig: FormFieldConfig[] = [];
+  onSubmit(formData: any): void {
+    throw new Error('Method not implemented.');
+  }
 }
