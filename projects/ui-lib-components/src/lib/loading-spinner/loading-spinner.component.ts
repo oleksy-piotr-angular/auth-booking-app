0992328ad75e8e41e6f0a1595265d8394e040da4
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-30 16:30:32 +0200

    Init UI librarry with components for common elements in _shell-app

---
+++ projects/ui-lib-components/src/lib/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,11 @@
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-loading-spinner',
+  standalone: true,
+  imports: [],
+  template: '<p>Loading spinner works!</p>',
+  styles: '',
+})
+export class LoadingSpinnerComponent {}
---
+++ projects/ui-lib-components/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ui-lib-components
+ */
+
+export * from './lib/loading-spinner/loading-spinner.component';
