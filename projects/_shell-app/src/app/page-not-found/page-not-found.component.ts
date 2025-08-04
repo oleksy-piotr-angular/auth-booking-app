ac50b31ef56daf2acb187a015db2c0ac1537fbdc
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-08-05 01:56:07 +0200

    feat(shell-app): scaffold HomePageComponent and PageNotFoundComponent
    - create HomePageComponent under src/app/home-page
    - create PageNotFoundComponent under src/app/page-not-found

---
+++ projects/_shell-app/src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [],
+  template: ` <p>Page not found!</p> `,
+  styles: ``,
+})
+export class PageNotFoundComponent {}
