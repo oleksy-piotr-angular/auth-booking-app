4d67c08c683f46090a1d359748f196de58940348
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-08-04 18:26:10 +0200

    chore(http): register authInterceptor via withInterceptors
    - import withInterceptors in shell and auth-mfe
    - wrap provideHttpClient() calls to use authInterceptor

--- projects/_shell-app/src/app/app.config.ts
+++ projects/_shell-app/src/app/app.config.ts
@@ -2,17 +2,21 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { HOST_ROUTES } from './host.routes';
 import { environment } from '@booking-app/environments/environment';
 import { AUTH_API_BASE } from '@booking-app/tokens';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(HOST_ROUTES),
     provideAnimations(),
-    provideHttpClient(),
+    provideHttpClient(
+      // run authInterceptor on every request
+      withInterceptors([authInterceptor])
+    ),
     {
       provide: AUTH_API_BASE,
       useValue: environment.apiBase,
--- projects/auth-mfe/src/app/app.config.ts
+++ projects/auth-mfe/src/app/app.config.ts
@@ -1,16 +1,21 @@
+// projects/auth-mfe/src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { AUTH_ROUTES } from './auth.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AUTH_API_BASE } from '@booking-app/tokens';
 import { environment } from '@booking-app/environments/environment';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(AUTH_ROUTES),
-    provideHttpClient(),
+    provideHttpClient(
+      // run authInterceptor on every request
+      withInterceptors([authInterceptor])
+    ),
     { provide: AUTH_API_BASE, useValue: environment.apiBase },
     provideAnimations(),
   ],
