9d72af59fc4c35ddc7b422ff0245932b463d4918
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-08-04 23:45:35 +0200

    feat(search-mfe): define default route to AppComponent

--- projects/search-mfe/src/app/search.routes.ts
+++ projects/search-mfe/src/app/search.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const SEARCH_ROUTES: Routes = [];
+export const SEARCH_ROUTES: Routes = [{ path: '', component: AppComponent }];
