1c2671dae167301bc76beb67e3365d2257c492da
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-08-05 01:56:18 +0200

    feat(auth-mfe): scaffold PageNotFoundComponent
    - create PageNotFoundComponent under src/app/page-not-found

---
+++ projects/auth-mfe/src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-page-not-found',
+  standalone: true,
+  imports: [],
+  template: ` <p>Page not found!</p> `,
+  styles: ``,
+})
+export class PageNotFoundComponent {}
